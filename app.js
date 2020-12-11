47d1c0d2bae347ef3e743d7c17f46bfc9f4e283d
Juliana Mattar <juliana.mattar@gmail.com>
2020-12-11 16:08:16 -0300

    teste

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+require("dotenv").config;
 const express = require("express");
 const cors = require("cors");
-
-const PORT = 1234;
+require("./config/db.config")();
 
 const app = express();
 
 app.use(express.json());
+// Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
 
-app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
+app.use(cors({ origin: process.env.REACT_APP_URL }));
+require("./config/passport.config")(app);
+
+const userRouter = require("./routes/user.routes");
+app.use("/api", userRouter);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server up and running at port ${process.env.PORT}`)
+);
